0f0879f6f886c96e102af6b5196df6e2892722bc
mshenoy0807 <meghashenoy87@gmail.com>
2025-01-14 17:06:12 -0600

    AAFHHHHH
    AHGGHHHGHGH did I do it

--- addiscafe_web/src/components/About.js
+++ addiscafe_web/src/components/About.js
@@ -1,18 +1,65 @@
 import React from "react";
+import './../css/homepage.css';
+
+function Carousel() {
+
+    return (
+        <div id="myCarousel" class="carousel slide carousel-page" data-ride="carousel">
+
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img class="image" src={require("../images/store_front/outside.JPG" )}alt="Image" />  
+                </div>
+
+                <div class="item">
+                    <img class="image" src={require("../images/store_front/front_counter.JPG" )} alt="Image" />
+                </div>
+            </div>
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    );
+};
+
 
 const About = () => {
     return (
-        <div className="row justify-content-center">
-            <div className="col-sm-5">
-                <h2>Location: 801 S Oak Park Avenue, Oak Park IL</h2>
-                <h2>Hours open</h2>
-            </div>
-            <div className="col-sm-5">
-                <h1>Est. 2016</h1>
-                <h2>More Business Card info</h2>
-                <h2></h2>
+        <div class="container-fluid my-4">
+            <div className="row justify-content-center about">
+                <div class="col-sm-6">
+                    <Carousel />
+                </div>
+                <div class="col-sm-6">
+                    <h2>About</h2>
+                    <div className="col-sm-6">
+                        <h2>Location:</h2>
+                        <h5>801 S Oak Park Avenue, Oak Park IL</h5>
+                        <h2>Hours open</h2>
+                        <h5>Closed on Mondays</h5>
+                        <h5>7am - 2pm Tuesday - Sunday</h5>
+                    </div>
+                    <div className="col-sm-6">
+                        <h2>Email: </h2>
+                        <div>
+                            <h2>Phone: </h2>
+                            <h5>(708) 712-1558</h5>
+                        </div>
+                        <h1>Est. 2016</h1>
+                    </div>
+                </div>
             </div>
         </div>
+        
     );
 }
 
--- addiscafe_web/src/components/Footer.js
+++ addiscafe_web/src/components/Footer.js
@@ -2,11 +2,24 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer class="text-center bg-custom-red">
-            <h1>Logo</h1>
-            <p>Addis Cafe</p>
-            <a href="#/contact"><p>Follow Us</p></a>
-            <p>Photo Credits: Emma Joy Balma</p>
+        <footer class="text-center text-center footer">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h1>Logo</h1>
+                    <p>Addis Cafe</p>
+                    <a href="#/contact"><p>Follow Us</p></a>
+                    <p>Photo Credits: Emma Joy Balma</p>
+                </div>
+                <div class="col-sm-4 d-flex ">
+                        <h1><a><i class="bi bi-facebook"></i></a></h1>
+                        <h1><a><i class="bi bi-yelp"></i></a></h1>
+                        <h1><a><i class="bi bi-instagram"></i></a></h1>
+                </div>
+                <div class="text-center p-3 copyright">
+                    © 2024 Copyright:
+                    <p><a class="text-dark" href="https://github.com/mshenoy87/AddisCafe">Created by: Megha Shenoy</a></p>
+                </div>
+            </div>
         </footer>
     );
 }
