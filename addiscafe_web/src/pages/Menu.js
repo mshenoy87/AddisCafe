c27f7ca5ac2cbe3e36d88e1769604033fc156e90
mshenoy0807 <msheno3@uic.edu>
2025-02-05 16:46:14 -0600

    Please please please
    don't prove I'm riiiight!
    And
    please please please don't bring me to tears when I just did my code so nice

--- addiscafe_web/src/pages/Menu.js
+++ addiscafe_web/src/pages/Menu.js
@@ -1,35 +1,80 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "../css/menu.css";
+// import {axios} from 'axios';
 
-import {axios} from 'axios';
+function ShoppingCart({cart}) {
 
-function ShoppingCart(props) {
+    // calculates the total price of items in cart and rounds it to two decimal places
+    const calculateTotal = () => {
+        var total = 0;
+        for (var i = 0; i < cart.length; i++) {
+            total += parseFloat(cart[i].price) * parseInt(cart[i].quantity);
+        }
+        // total = cart.array.forEach(item => {parseFloat(item.price) * parseInt(item.quantity)});
+        return total.toFixed(2);
 
+    }
     return (
-        <div class="">
+            <div className="bg-gray rounded-5 py-1">
+                <div className="h1">Shopping Cart</div>
 
+                {cart.length === 0 ? (
+                    <p>Your cart is empty</p>
+                ) : (
+                    <ul className="px-4 my-5">
+                    {cart.map((c, index) => (
+                        <div className="row p-5 m-2 rounded-5 bg-green yellow-text text-center" key={index}>
+                            <div className="h4 col-xs-12 col-sm-4 col-md-12 text-center">{c["name"]}: </div>
+                            {c["variation"] ? (
+                                <div class="col">
+                                    <div className="h4 col-xs-4 col-sm-4 col-md-4 text-center">{c["variation"]}</div>
+                                    <div className="h4 col-xs-5 col-sm-4 col-md-4 text-center">{c["quantity"]}x </div>
+                                    <div className="h4 col-xs-3 col-sm-4 col-md-4 text-center">${c["price"]} {"/each"}</div>
+                                </div>
+                                ) : (
+                                <div className="col">
+                                    <div className="h4 col-xs-5 col-sm-5 col-md-5 text-center">{c["quantity"]}x = </div>
+                                    <div className="h4 col-xs-7 col-sm-7 col-md-7 text-center">${c["price"]} {"/each"}</div>
+                                </div>
+                            )}
+                            <div class="col-xs-1 text-center btn-outline-none btn-sm remove-from-cart">
+                                <i className="bi bi-x h2"></i>
+                            </div>
+                        </div>
+                        
+                    ))}
+                    </ul>
+                )}
+                <div className="my-2 h3">Total: ${calculateTotal()}</div>
         </div>
     )
-
 }
 
-function ItemCard(props) {
-    const item = props.item;
+function ItemCard({item, addToCart}) {
+    const [variation, setVariation] = useState(item.variations[0]);
 
     return (
         <div key={item.name} style={{height: "100%"}}>
-            <div class="h1">{item.name}</div>
-            {(item.variations.length > 1 ? (
-                <div class="dropdown-group">
-                    <select title="variations" class="rounded-pill p-1" style={{color: "#002255"}}>
+            <div className="h1">{item.name}</div>
+            {(item.variations.length > 1 && (
+                <div className="dropdown-group">
+                    <select title="variations" 
+                            className="rounded-pill p-1"
+                            style={{color: "#002255"}}
+                            onChange={(e) => setVariation(item.variations.find((v) => v.name === e.target.value))}>
                     {item.variations.map((variation) => (
                         <option value={variation.name} key={variation.name} >{variation.name}</option>         
                         ))
                     }
                     </select>
                 </div>
-                ) : null
+                )
             )}
+            <button className="btn btn-lg bg-blue brown-text rounded-pill add-to-cart my-3" 
+                onClick={() => {addToCart({ name: item.name, variation: (variation.name !== "Regular" && variation.name), price: variation.price })}}>
+                    Add to Cart
+            </button>
+
         </div>
     );
 }
@@ -37,9 +82,9 @@ function ItemCard(props) {
 
 
 
-const MenuPage = () => {
-    // const [data, setData] = useState(null);
+function MenuPage() {
     const [cart, setCart] = useState([]);
+    // const [data, setData] = useState(null);
     
 
     // useEffect(() => {
@@ -51,58 +96,49 @@ const MenuPage = () => {
     // static website doesn't allow for axios calls from the API so we need to save the data and get it in a JSON file
     const sample = require('../build/menu.json');
 
-    const addToCart = (event) => {
-        event.preventDefault();
+    const addToCart = (item) => {
+        setCart((prevCart) => {
+            const existsInCart = prevCart.findIndex(
+                (cartItem) => cartItem.name === item.name && cartItem.variation === item.variation
+            );
 
-        // parent div where the button was clicked at
-        const parent = event.target.parentElement;
-
-        // item data to be passed into the shopping cart
-        var item = {
-            name: parent.querySelector(".h1").innerText,
-            variation: parent.querySelector("select") ? parent.querySelector("select").value : ""
-        }
-
-        setCart((cart) => [...cart, item]);
-    
-    }
+            if (existsInCart !== -1) {    // not found in cart
+                return prevCart.map((cartItem, index) =>
+                    index === existsInCart
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                        : cartItem
+                );
+            } else {
+                return [...prevCart, { ...item, quantity: 1 }];
+            }
+        });
+    };
 
     return (
-        <div class="container-fluid row">
-            <div className="col-sm-12 col-md-4 rounded-5 my-4">
-                <div class="h1">Shopping Cart</div>
-                <div class="bg-gray rounded-5">
-                    {cart.length === 0 ? (
-                        <p>Your cart is empty</p>
-                    ) : (
-                        <ul className="px-4 py-5 my-5">
-                        {cart.map((c) => (
-                            <div class="row p-4 m-3 rounded-5 bg-green yellow-text">
-                                <div class="h4 col-lg-4">{c["name"]}: </div>
-                                <div class="h5 col-lg-4">{c["variation"] ? c["variation"] : ""}</div>
-                                <div class="h4 col-lg-4">x1 = </div>
-                            </div>
-                        ))}
-                        </ul>
-                    )}
-                    <p className="my-2">Total: ${0}</p>
-                </div>
+        <div className="container-fluid row text-center">
+            <div className="col-sm-12 col-md-4 rounded-5 my-5">
+                <ShoppingCart cart={cart} />
             </div>
+            
+            
             <div className="col-sm-12 col-md-8 my-4">
-                <h1>(MOCK) MENU</h1>
-                {sample ? (sample.map((item) => (
-                        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 p-3">
-                            <div class="item-card bg-yellow green-text rounded-5 py-5">
-                                <ItemCard item={item} isMenuItem/>
-                                <button class="btn btn-lg bg-blue brown-text rounded-pill add-to-cart my-3" onClick={addToCart}>Add to Cart</button>
+                <h1>MENU</h1>
+
+                <div className="row">
+                    {sample ? (sample.map((item, index) => (
+                            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-3 p-3" key={index}>
+                                <div className="item-card bg-yellow green-text rounded-5 py-5">
+                                    <ItemCard item={item} addToCart={addToCart}/>
                             </div>
                         </div>
                     ))
                 ) : (
-                    <div class="spinner-grow" role="status">
-                        <span class="visually-hidden">Loading...</span>
+                            <div className="spinner-grow" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        )
+                    }
                     </div>
-                )}
             </div>
         </div>
     );
